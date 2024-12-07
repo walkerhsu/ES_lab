2bcfafb7ad2b44db8451142b772cdc67c58c3dd9
Bowen <bowen0921@gmail.com>
2024-12-07 21:12:35 +0800

    receive serial data

---
+++ Final_project/Final/Data_receiver.py
@@ -0,0 +1,12 @@
+import serial
+
+# Configure the serial port
+ser = serial.Serial('COM4', baudrate=115200, timeout=1)  # Replace 'COM3' with your port
+output_file = "Angular_velocity_y.txt"
+
+with open(output_file, 'w') as file:
+    while True:
+        data = ser.readline().decode('utf-8')  # Read a line from STM32
+        if data:
+            print(data.strip())  # Display on console
+            file.write(data.strip() + "\n")    # Write to file
