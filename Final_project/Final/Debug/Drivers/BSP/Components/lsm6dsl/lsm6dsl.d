2d61f1d8f7a3cf94cac352330e7ebe43dd38eb80
dienruei123 <dienruei@gmail.com>
2024-12-07 18:48:24 +0800

    Final STM32 init + BLE, gyro

---
+++ Final_project/Final/Debug/Drivers/BSP/Components/lsm6dsl/lsm6dsl.d
@@ -0,0 +1,8 @@
+Drivers/BSP/Components/lsm6dsl/lsm6dsl.o: \
+ C:/Users/User/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/lsm6dsl.c \
+ C:/Users/User/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/lsm6dsl.h \
+ C:/Users/User/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/../Common/accelero.h \
+ C:/Users/User/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/../Common/gyro.h
+C:/Users/User/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/lsm6dsl.h:
+C:/Users/User/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/../Common/accelero.h:
+C:/Users/User/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/../Common/gyro.h:
